26ae2b282e8a5c3eb7ef712f629633576053e1fd
Azaliia Kasimova <sea-blue@inbox.ru>
2022-12-18 00:58:51 +0400

    task 1.2

--- task_1.2.js
+++ task_1.2.js
@@ -1,11 +1,17 @@
-const csv = require("csvtojson");
+const csvtojson = require("csvtojson");
 const fs = require("fs");
 
-const csvFilePath = "./csv/data.csv";
+const rStream = fs.createReadStream("./csv/data.csv");
+const wStream = fs.createWriteStream("./data.txt", "utf8");
 
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    let data = JSON.stringify(jsonObj);
-    fs.writeFileSync("data.txt", data);
-  });
+rStream.pipe(csvtojson()).on("data", (chunk) => {
+  wStream.write(chunk.toString());
+});
+
+rStream.on("error", (error) => {
+  console.error(error.message);
+});
+
+wStream.on("error", (error) => {
+  console.error(error.message);
+});
