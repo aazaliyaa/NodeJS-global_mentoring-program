3ad9beb1828392b30bcb03c2b806ea07006774bc
Azaliia Kasimova <sea-blue@inbox.ru>
2022-12-31 22:35:58 +0400

    create server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+// default URL to API
+app.use("/", function (req, res) {
+  res.send("node-ex-api works :-)");
+});
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+
+console.debug("Server listening on port " + port);
